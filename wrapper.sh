9dd82941eead8053bdd19a65f383da2d12bd015b
Guest257351 <lukenicholl510@gmail.com>
2021-12-21 01:15:52 +1030

    new about page

--- wrapper.sh
+++ wrapper.sh
@@ -4,7 +4,9 @@
 # launch the flask server and put it in the background
 export FLASK_APP=flask_app
 
-flask run -h 0.0.0.0 -p 80
+# flask run -h 0.0.0.0 -p 80 --cert=adhoc
+
+waitress-serve --call 'flask_app:create_app' --port=80 --url-scheme=https
 
 wait -n
 
